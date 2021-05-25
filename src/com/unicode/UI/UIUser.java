e6ac604ddc0d32d2ae48daf146f2c59726f4b399
DanielAristy <danielaristy22@gmail.com>
2021-05-24 19:25:01 -0500

    Ingresando datos por teclado en el cual se define cual es mayor, menor, o son iguales

--- src/com/unicode/UI/UIUser.java
+++ src/com/unicode/UI/UIUser.java
@@ -1,7 +1,13 @@
 package com.unicode.UI;
 
-public abstract class UIUser {
+import java.util.Scanner;
 
+public abstract class UIUser {
+    private static final String ENTER_THE_VALUE = "Ingrese el valor del numero";
+    private static final String THEY_ARE_EQUAL = "Los numeros son iguales";
+    private static final String GREATHER_THAN = " mayor que ";
+    private static final String SMALLER_THAN = " menor que ";
+    private static final Scanner sc = new Scanner(System.in);
     public static void showMenu(){
         greaterOrEqualNumber();
     }
@@ -9,15 +15,29 @@ public abstract class UIUser {
     //Ejercicios del taler
 
     //1. Numero mayor o igual
+    //2. Ingresar por consola cual de los numeros es mayor, menor o igual
     private static void greaterOrEqualNumber(){
-        int numberOne = 20;
-        int numberTwo = 10;
+        showMessage(ENTER_THE_VALUE);
+        int numberOne = sc.nextInt();
+        showMessage(ENTER_THE_VALUE);
+        int numberTwo = sc.nextInt();
 
-        if (numberOne == numberTwo){
-            showMessage("Los numeros son iguales");
-        }else if (numberOne > numberTwo){
-            showMessage("El numero " + numberOne + " es mayor que " + numberTwo);
-        }else showMessage("El numero " + numberTwo + " es mayor que " + numberOne);
+        if (isEquals(numberOne, numberTwo)){
+            showMessage(THEY_ARE_EQUAL);
+        }else if (theNumberIsGreater(numberOne, numberTwo)){
+            showMessage(numberOne + GREATHER_THAN + numberTwo);
+        }else {
+            showMessage(numberTwo + GREATHER_THAN + numberOne);
+            showMessage(numberOne + SMALLER_THAN + numberTwo);
+        }
+    }
+
+    private static boolean theNumberIsGreater(int numberOne, int numberTwo) {
+        return numberOne > numberTwo;
+    }
+
+    private static boolean isEquals(int numberOne, int numberTwo) {
+        return numberOne == numberTwo;
     }
 
     private static void showMessage(String message){
